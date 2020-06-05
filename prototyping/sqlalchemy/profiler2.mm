4f4c584a78a1c66802a12b6c868850744ea73d8c
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-06-05 15:29:28 -0400

    Create profiler2.mm

---
+++ prototyping/sqlalchemy/profiler2.mm
@@ -0,0 +1,53 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1591366855410" ID="ID_111163573" MODIFIED="1591367544114" STYLE="fork" TEXT="profiler2">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1591366886183" ID="ID_1462147570" MODIFIED="1591367333988" POSITION="right" TEXT="core (profiler2 py3 module)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1591367033328" ID="ID_1679026549" MODIFIED="1591367120688" TEXT="fake AP beacons">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1591367041288" ID="ID_478953728" MODIFIED="1591367120689" TEXT="sniffer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1591367046228" ID="ID_26277921" MODIFIED="1591367120693" TEXT="profile captured assoc&apos;s">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1591366897042" ID="ID_1804906096" MODIFIED="1591367120698" POSITION="left" TEXT="backend (database)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1591366925860" ID="ID_752682553" MODIFIED="1591367505478" TEXT="SQLAlchemy (SQL toolkit / ORM)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1591366965285" ID="ID_1872727822" MODIFIED="1591367120704" TEXT="sqlite as the db">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1591367140598" ID="ID_871125694" MODIFIED="1591367522532" TEXT="schema/versioning?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1591367513359" ID="ID_1243808405" MODIFIED="1591367516747" TEXT="tables?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1591366903994" ID="ID_640613988" MODIFIED="1591367120706" POSITION="right" TEXT="frontend (webui)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1591366980575" ID="ID_652460464" MODIFIED="1591367120709" TEXT="v1. cockpit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1591366991852" ID="ID_1617967165" MODIFIED="1591367120711" TEXT="v2. coupled to what?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
