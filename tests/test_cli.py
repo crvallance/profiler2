a7f0b315d584459ae462b7bdd2a407f7632d96f8
Colin Vallance <crvallance@gmail.com>
2020-07-09 21:41:42 +0000

    more tests to cover all flags

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,20 +23,60 @@ no_input_flags = [
 ]
 
 input_needed = [
-    '-i INTERFACE',
-    '-c CHANNEL',
-    '-s SSID',
     '--file <FILE>',
     '--config <FILE>',
     '--menu_file <FILE>',
     '--files_root <PATH>',
-    '--logging [{debug,warning}]' 
 ]
 
+debug_types = ['debug', 'warning']
 
 @pytest.mark.parametrize('flag', no_input_flags)
 def test_cli_no_args(flag):
     output = subprocess.Popen([insertion, flag], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"
+
+
+def test_channel_flag():
+    output = subprocess.Popen([insertion, '-c 3'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"
+
+
+def test_ssid_flag():
+    output = subprocess.Popen([insertion, '--ssid', ' pytest'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"
+
+
+
+@pytest.mark.parametrize('debug', debug_types)
+def test_logging_flag(debug):
+    output = subprocess.Popen([insertion, '--logging', debug], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"
+
+
+
+def test_interface_flag():
+    output = subprocess.Popen([insertion, '-i wlan0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
         output.wait(15)
     except subprocess.TimeoutExpired:
