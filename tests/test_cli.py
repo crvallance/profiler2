6fe2c138edf044ce6cae73016a0d1e65dd0d2bdd
Colin Vallance <crvallance@gmail.com>
2020-07-10 02:01:00 +0000

    closing the test gap

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,9 +2,10 @@
 
 import pytest
 import subprocess
-import pathlib
-import time
 import signal
+import pathlib
+from profiler2 import helpers
+import sys
 
 insertion = '/usr/local/bin/profiler'
 
@@ -19,17 +20,50 @@ no_input_flags = [
     '--version',
     '-V',
     '-h',
-    '--help'
+    '--help',
+    '--11r',
+    '--11ax',
 ]
 
 input_needed = [
     '--file <FILE>',
-    '--config <FILE>',
     '--menu_file <FILE>',
     '--files_root <PATH>',
+    '--pcap <FILE>',
 ]
 
+setup_needed = '--noprep'
+
 debug_types = ['debug', 'warning']
+
+@pytest.fixture()
+def setup_interface():
+    # log.info("start interface prep...")
+    if not helpers.prep_interface('wlan0', "monitor", '6'):
+    # if not helpers.prep_interface(interface, "monitor", channel):
+        # log.error("failed to prep interface")
+        print("exiting...")
+        sys.exit(-1)
+        # log.info("done prep interface...")
+
+def test_no_prep(setup_interface):
+    output = subprocess.Popen([insertion, '--noprep'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"
+
+def test_config_input():
+    config_ini = pathlib.Path('./profiler2/config.ini')
+    output = subprocess.Popen([insertion, '--config', config_ini], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        output.wait(15)
+    except subprocess.TimeoutExpired:
+        output.send_signal(signal.SIGINT)
+        output.wait()
+    assert output.returncode == 0, "Program did not exit cleanly with no args"    
 
 @pytest.mark.parametrize('flag', no_input_flags)
 def test_cli_no_args(flag):
@@ -62,7 +96,6 @@ def test_ssid_flag():
     assert output.returncode == 0, "Program did not exit cleanly with no args"
 
 
-
 @pytest.mark.parametrize('debug', debug_types)
 def test_logging_flag(debug):
     output = subprocess.Popen([insertion, '--logging', debug], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -72,7 +105,6 @@ def test_logging_flag(debug):
         output.send_signal(signal.SIGINT)
         output.wait()
     assert output.returncode == 0, "Program did not exit cleanly with no args"
-
 
 
 def test_interface_flag():
